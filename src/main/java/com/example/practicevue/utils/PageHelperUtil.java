d9d2866334ca4c69da0b3d19b490455e802ae8b8
caozecun <vzwkbBLifviKihv6QYiy>
2021-03-02 16:12:18 +0800

    完成代码实现

--- src/main/java/com/example/practicevue/utils/PageHelperUtil.java
+++ src/main/java/com/example/practicevue/utils/PageHelperUtil.java
@@ -11,17 +11,18 @@ import java.util.function.Supplier;
  * @date 2020/10/28
  */
 public class PageHelperUtil {
+
     /**
      * 分页工具
      * @param pageNum   起始页
      * @param pageSize  页面容量
-     * @param supplier  即将查询分页的方法
-     * @param <T>       实体类型
-     * @return 将查询分页之后的结果
+     * @param supplier  即将查询的方法
+     * @param <T>       查询返回的实体类型
+     * @return 将查询结果分页之后的数据
      */
     public static <T> PageInfo<T> page(int pageNum, int pageSize, Supplier<List<T>> supplier){
         PageHelper.startPage(pageNum, pageSize == 0 ? 10 : pageSize);
-        PageInfo<T> pageInfo = new PageInfo<T>(supplier.get());
+        PageInfo<T> pageInfo = new PageInfo<>(supplier.get());
         PageHelper.clearPage();
         return pageInfo;
     }
